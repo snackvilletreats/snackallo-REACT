1cd9eadea20d6c5464b67a1b53c74c33a57f1605
snackvilletreats <snackvilletreatsllp@gmail.com>
2025-07-24 07:12:39 +0530

    Create Cart.js

---
+++ frontend/src/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Your Cart</h2>
+      <p>Cart functionality will be integrated with backend session or localStorage.</p>
+    </div>
+  );
+}
+
+export default Cart;
