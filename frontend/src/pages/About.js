b468be9d0ac5b6a00b29fa6e971f3aeab1de54ca
snackvilletreats <snackvilletreatsllp@gmail.com>
2025-07-24 07:11:57 +0530

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,5 @@
+import React from 'react';
+function About() {
+  return <div style={{ padding: 20 }}><h2>About Snackallo</h2><p>We deliver tasty banana chips and traditional snacks.</p></div>;
+}
+export default About;
